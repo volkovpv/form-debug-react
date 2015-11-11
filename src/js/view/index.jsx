7ab6b6f3735c67705abefb4c195a9c8016267749
Пётр <p.v.volkov@yandex.ru>
2015-11-12 00:21:47 +0300

    add data

--- src/js/view/index.jsx
+++ src/js/view/index.jsx
@@ -5,12 +5,32 @@
 var Footer  = require('./Footer'),
     Wrap    = require('./Wrap');
 
+var Select = require('react-select');
+
 var Index = React.createClass({
+    _logChange: function(val){
+        alert("Selected: " + val)
+    },
+
     render: function(){
+        var options = [
+            { value: 'one', label: 'One' },
+            { value: 'two', label: 'Two' }
+        ];
+
         return(
             <div id="main">
                 <Wrap/>
+
+                <Select
+                    name="form-field-name"
+                    value="one"
+                    options={options}
+                    onChange={this._logChange}
+                    />
+
                 <Footer/>
+
             </div>
         )
     }
